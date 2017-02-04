85f4982d491ee5a71948c8901cdc8749e35fbb86
Daniel Lin <danlin604@gmail.com>
2017-02-03 18:08:02 -0800

    Updated node.js notes
    Signed-off-by: Daniel Lin <danlin604@gmail.com>

--- NodeJS.js
+++ NodeJS.js
@@ -6,8 +6,6 @@ setTimeout(function() {
     console.log('3 seconds have passed');
 }, 3000);
 
-
-
 /*******************************************************
 * 
 *******************************************************/
@@ -450,21 +448,392 @@ npm install express
 
 npm uninstall express
 
+/* Package management */
+
+npm init
+
+	name:
+	version:
+	description:
+	entry point:
+	test command:
+	git repository:
+	keywords:
+	license:
+	...
+
+npm install express -save
+
+	save dependencies to our package.json
+
+npm uninstall express
+
+	dependency on express remains in package.json
+
+npm inatll 
+
+	installs all dependencies
+
+
+/*******************************************************
+* Nodemon
+*******************************************************/
+
+/* Monitor application files, restarts server when file modified */
+
+npm install -g nodemon 
+
+	-g means global install
+
+// node app.js --> nodemon app.js
+
+nodemon app
+
+/*******************************************************
+* Responding to requests
+*******************************************************/
+
+GET
+
+	app.get('route', fn)
+
+POST
+
+	app.post('route', fn)
+
+DELETE
+
+	app.delete('route', fn)
+
+
+
+
+
+/* quick routing */
+
+const express = require('express')
+const app = express() // express methods
+
+app.get('/', function(req, res) {
+    res.send('this is the homepage')
+})
+
+app.get('/contact', function(req, res) {
+    res.send('this is the contact page')
+})
+
+app.listen(3000)
+
+
+
+/*******************************************************
+* Express Routing
+*******************************************************/
+
+app.get('/profile/:id', function(req, res) {
+    res.send(`you requested to see a profile with the id of ${req.params.id}`)
+})
+
+
+
+
+/*******************************************************
+* Templating/View Engine: EJS
+*******************************************************/
+
+npm install ejs -save
+
+app.set('view engine', 'ejs') // ejs as express view engine
+
+/* /views/profile.ejs */
+
+<body>
+    <h1>Welcome to the profile of <%= person %></h1>
+    <p><strong>Age: </strong><%= data.age %></p>
+    <p><strong>Job: </strong><%= data.job %></p>
+</body>
+
+/* app.js */
+
+app.get('/profile/:name', function(req, res) {
+    let data = {
+        age: 60,
+        job: 'coder'
+    }
+    res.render('profile', {
+        person: req.params.name,
+        data: data
+    })
+})
+
+
+
+
+
+/* loop */
+
+<h2>Hobbies</h2>
+<ul>
+    <% data.hobbies.forEach(function(item) { %>
+        <li>
+            <%= item %> 
+        </li>
+    <% }) %>
+</ul>
+
+app.get('/profile/:name', function(req, res) {
+    
+    let data = {
+        age: 60,
+        job: 'coder',
+        hobbies: ['eating', 'belching', 'napping']
+    }
+
+    res.render('profile', {
+        person: req.params.name,
+        data: data
+    })
+})
+
+
+
+
+/* views/partial/nav.ejs */
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
+</nav>
+
+
+/* views/index.ejs */
+
+...
+<body>
+    <% include partials/nav.ejs %>
+...
+
+
+/* app.js */
+app.get('/', function(req, res) {
+    res.render('index')
+})
+
+
+
+
+/*******************************************************
+* Express middleware & static files (css)
+*******************************************************/
+
+/* index.html */
+<link href="/assets/styles.css" rel="stylesheet" type="text/css" />
+
+/* assets/styles.css */
+body { background: skyblue; font-family: verdana; color: #fff; padding: 30px; }
+h1 { font-size: 48px; text-transform: uppercase; letter-spacing: 2px; text-align: center; }
+h2 { font-size: 30px; text-transform: uppercase; letter-spacing: 2px; text-align: center; }
+p, li { font-size: 16px; text-align: center; }
+
+/* app.js */
+app.use('/assets', express.static('assets')) // use middleware
+
+
+
+/*******************************************************
+* Query String
+*******************************************************/
+
+/* http://127.0.0.1:3000/contact?dept=5&person=joe */
+
+app.get('/contact', function(req, res) {
+    console.log(req.query) // { dep: '5', person: 'joe' }
+    res.render('contact')
+})
+
+
+
+
+res.render('contact', { qs: req.query })
+
+/* Call query-string object passed by the .render method
+
+	<p><%= qs.dept %></p>
+	<p><%= qs.person %></p>
+*/
+
+/* Form
+
+    <form id="contact-form">
+        <label for="who">Who do you want to contact?</label>
+        <input type="text" name="who" value="<%= qs.person %>" />
+        <label for="department">Which department?</label>
+        <input type="text" name="department" value="<%= qs.dept %>" />
+        <label for="email">Your email</label>
+        <input type="email" name="email" />
+        <input type="submit" value="submit" />
+    </form>
+*/
+
+
+/*******************************************************
+* Express body-parser middleware
+*******************************************************/
+
+/* url: https://www.npmjs.com/package/body-parser */
+
+npm install body-parser -save
+
+
+/*	POST form
+    <form id="contact-form" method="POST" action="/contact">
+        <label for="who">Who do you want to contact?</label>
+        <input type="text" name="who" value="<%= qs.person %>" />
+        <label for="department">Which department?</label>
+        <input type="text" name="department" value="<%= qs.dept %>" />
+        <label for="email">Your email</label>
+        <input type="email" name="email" />
+        <input type="submit" value="submit" />
+    </form>
+*/
+
+app.post('/contact', urlencodedParser,  function(req, res) {
+    console.log(req.body) // { who: 'dan', department: 'IT', email: 'dan@gmail.ca' }
+    res.render('contact-success', { data: req.body })
+})
+
+/*
+    <p>You contacted <%= data.who %> in the <%- data.department%> department.</p>
+    <p>We will reply to you at <%= data.email %></p>    
+*/
+
+/*******************************************************
+* Todo List
+*******************************************************/
+
+/* todoController.js */
+
+const bodyParser = require('body-parser')
+
+let data = [
+    {item: 'get milk'},
+    {item: 'walk dog'},
+    {item: 'kick ass'}
+]
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+module.exports = function(app) {
+
+    app.get('/todo', function(req, res) {
+        res.render('todo', {todos: data})
+    })
+
+    app.post('/todo', urlencodedParser, function(req, res) {
+        data.push(req.body)
+        res.json(data)
+    })
+
+    app.delete('/todo/:item', function(req, res) {
+        data = data.filter(function(todo) {
+            return todo.item.replace(/ /g, '-') !== req.params.item
+        })
+        res.json(data)
+    })
+}
+
+/* todo-list.js - jquery*/
+
+$(document).ready(function(){
+
+  $('form').on('submit', function(){
+
+      var item = $('form input');
+      var todo = {item: item.val()};
+
+      $.ajax({
+        type: 'POST',
+        url: '/todo',
+        data: todo,
+        success: function(data){
+          //do something with the data via front-end framework
+          location.reload();
+        }
+      });
+
+      return false;
+
+  });
+
+  $('li').on('click', function(){
+      var item = $(this).text().replace(/ /g, "-");
+      $.ajax({
+        type: 'DELETE',
+        url: '/todo/' + item,
+        success: function(data){
+          //do something with the data via front-end framework
+          location.reload();
+        }
+      });
+  });
+
+});
+
+/* todo.ejs 
+
+    <h1>My Todo List</h1>
+    <div id="todo-table">
+        <form>
+            <input type="text" name="item" placeholder="Add new item..." required />
+            <button type="submit">Add Item</button>
+        </form>
+        <ul>
+            <% for(let i = 0; i < todos.length; i++) { %>
+                <li><%= todos[i].item %></li>
+            <% } %>
+        </ul>
+    </div>
+
+    <script src="/assets/todo-list.js"></script>
+*/
+
+/* app.js */
+
+const express = require('express')
+const todoController = require('./controllers/todoController')
+
+const app = express() // express methods
+
+// setup template engine
+app.set('view engine', 'ejs')
+
+// static files
+app.use(express.static('./public'))
+
+// fire controllers
+todoController(app)
+
+// listen to port
+app.listen(3000)
+console.log('You are listening to port 3000')
+
 
 /*******************************************************
 * 
 *******************************************************/
 
 
+/*******************************************************
+* 
+*******************************************************/
+
 
 /*******************************************************
 * 
 *******************************************************/
 
 
-
 /*******************************************************
 * 
 *******************************************************/
-
-
