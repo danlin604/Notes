3b16fc84ec94767d74e07d0978acdbb1626091fc
Daniel Lin <danlin604@gmail.com>
2017-02-09 21:47:05 -0800

    Fun with prototypes
    Signed-off-by: Daniel Lin <danlin604@gmail.com>

--- Test/test.js
+++ Test/test.js
@@ -1,19 +1,42 @@
-function talk() {
-  console.log(this.sound)
-}
-let animal = {  
-  talk
-}
-let dog = {
-  sound: 'woof!'
-}
-let prarieDog = {
-  howl: function() {
-    console.log(this.sound.toUpperCase())
-  }
-}
-Object.setPrototypeOf(dog, animal)
-dog.talk() // woof!
-Object.setPrototypeOf(prarieDog, dog)
-prarieDog.howl() // WOOF!
+/* Prototypes delegates objects */
 
+/* 
+Analogy: you ask if your friend has a pen, and he asks his, and a pen is passed back to you 
+
+Object.__proto__ 
+
+  all objects will delegate to __proto__
+
+  Object.__proto__ === Object.prototype // true
+
+  Global Object() is actually a function
+
+Object.prototype
+
+  prototype is used when the user calls 'new', the term prototype should be prototypeToInstall or prototypeToUseInNew to be more descriptive
+
+Object.__proto__
+
+  __proto__ is property on an object that points out the prototype that has been set for that object
+
+*/
+
+
+let cat = {
+  breed: 'munchkin'
+}
+
+let myCat = {
+  name: 'Dreadlord Razorfluff'
+}
+
+Object.setPrototypeOf(myCat, cat)
+
+console.log(myCat.breed)
+
+// reference to the same object
+console.log(myCat.__proto__) // { breed: 'munchkin' } 
+
+cat.tailLength = 15
+
+console.log(myCat.__proto__) // { breed: 'munchkin', tailLength: 15 }
