43fe85f3b0b99c0a9213166892b48ddbf5bfa17b
Daniel Lin <danlin604@gmail.com>
2017-02-19 18:21:27 -0800

    More on CSS selectors
    Signed-off-by: Daniel Lin <danlin604@gmail.com>

--- Test/test.js
+++ Test/test.js
@@ -1,4 +1,6 @@
 
-function (sound) {
-  
-}
+let a = ['MySQL', 'HiveQL', 'C++', 'C', 'Node', 'Angular', 'React', 'PHP', 'Code Igniter', 'TypeScript', 'SVN', 'Jira', 'Agile', 'MVC', 'RESTful Service']
+
+a.sort()
+
+console.log(a)
