2f49e0ee46c4cb8b2bbc1ecba9975bbd3e2ee75a
Daniel Lin <danlin604@gmail.com>
2017-02-22 21:14:09 -0800

    More on CSS
    Signed-off-by: Daniel Lin <danlin604@gmail.com>

--- Test/test.js
+++
@@ -1,6 +0,0 @@
-
-let a = ['MySQL', 'HiveQL', 'C++', 'C', 'Node', 'Angular', 'React', 'PHP', 'Code Igniter', 'TypeScript', 'SVN', 'Jira', 'Agile', 'MVC', 'RESTful Service']
-
-a.sort()
-
-console.log(a)
